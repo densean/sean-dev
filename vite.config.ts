1976d6595d93e0bed1ed3c717f2a0dd3173c69a8
sean <siclot.denissean33@gmail.com>
2024-12-01 23:41:15 +0800

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+// vite.config.ts
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "build", // Change 'dist' to 'build'
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
